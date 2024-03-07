bc2dcd4010c5dc32d028b5600e7591c94caa99f2
Nkosinathi Prince Hiyashe <nphiyashe@gmail.com>
2024-03-07 15:09:36 +0000

    add an array containing quiz questions

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,27 @@
+const questions = [
+        {
+        title: 'What does the term "hoisting" refer to in JavaScript?',
+        choices: ['A) Moving variables to the top of their scope', 'B) Creating a new variable', 'C) Declaring a function', 'D) Sorting an array'],
+        answer: 'A) Moving variables to the top of their scope'
+        },
+        {
+        title: 'What is the purpose of the "this" keyword in JavaScript?',
+        choices: ['A) Refers to the current function', 'B) Refers to the global object', 'C) Refers to the parent object', 'D) Refers to the current instance of an object'],
+        answer: 'D) Refers to the current instance of an object'
+        },
+        {
+        title: 'Which of the following is NOT a valid way to declare a variable in JavaScript?',
+        choices: ['A) let myVar = 10;', 'B) const myVar = 20;', 'C) var myVar = 30;', 'D) myVar = 40;'],
+        answer: 'D) myVar = 40;'
+        },
+        {
+        title: 'What does the "NaN" value represent in JavaScript?',
+        choices: ['A) Not a Number', 'B) Null', 'C) Negative Infinity', 'D) Undefined'],
+        answer: 'A) Not a Number'
+        },
+        {
+        title: 'What is the purpose of the "map" method in JavaScript arrays?',
+        choices: ['A) Creates a new array with the results of calling a provided function on every element', 'B) Removes an element from an array', 'C) Sorts the array in ascending order', 'D) Checks if all elements satisfy a condition'],
+        answer: 'A) Creates a new array with the results of calling a provided function on every element'
+        }
+    ];
