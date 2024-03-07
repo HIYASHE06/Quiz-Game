d5c7a5d18420c2de42855331cb36fbfa4e933c17
Nkosinathi Prince Hiyashe <nphiyashe@gmail.com>
2024-03-07 15:16:09 +0000

    Starts the quiz by hiding the start screen, starting the timer, and displaying the questions section.

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -9,3 +9,13 @@ const submitBtn = document.getElementById('submit');
 const startBtn = document.getElementById('start');
 const initialsEl = document.getElementById('initials');
 const feedbackEl = document.getElementById('feedback');
+
+function startQuiz() {
+    // hide start screen
+    document.getElementById('start-screen').classList.add('hide');
+    // start timer
+    timerId = setInterval(clockTick, 1000);
+    // show questions section
+    questionsEl.classList.remove('hide');
+    getQuestion();
+}
